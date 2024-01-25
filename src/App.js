6ac63c43ffe5e95922c6638bd62233989edb363d
naman bansal <naman.namo1996@gmail.com>
2024-01-25 23:43:19 +0530

    adding navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './component/Navbar';
+import "./App.css"
+
+
+function App() {
+  return (
+   <>
+   <main>
+    <Navbar/>
+   </main>
+   </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <p>logo</p>
+      </div>
+      <input type='text'/>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="search w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+</svg>
+<div className='links'>
+  <ul>
+    <li>Statistics</li>
+    <li>overview</li>
+    <li>Dashboard</li>
+    <li>Analytics</li>
+  </ul>
+  </div>
+  <div className='profile'>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
+</svg>
+
+
+  </div>
+
+    </div>
+  )
+}
